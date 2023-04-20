48b19e70395f488d49bf81a5818074bc30cc3b32
unknown <ismayilrha@code.edu.az>
2023-04-20 21:14:45 +0400

    assigment

--- js/app.js
+++ js/app.js
@@ -15,3 +15,33 @@ function transformString(inputString) {
 
   let result = transformString('hello');
 console.log(result); 
+
+
+
+
+
+// assigment 2
+
+function dishes(recipe, availableIngredients) {
+  let maxDishes = Infinity;
+  for (let ingredient in recipe) {
+    if (availableIngredients[ingredient]) {
+      maxDishes = Math.min(maxDishes, Math.floor(availableIngredients[ingredient] / recipe[ingredient]));
+    } else {
+      maxDishes = 0;
+      break;
+    }
+  }
+  return maxDishes;
+}
+
+
+let recipe1 = {flour: 500, sugar: 200, eggs: 1};
+let availableIngredients1 = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
+let result1 = dishes(recipe1, availableIngredients1);
+console.log(result1); // 2
+
+let recipe2 = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+let availableIngredients2 = {sugar: 500, flour: 2000, milk: 2000};
+let result2 = dishes(recipe2, availableIngredients2);
+console.log(result2); // 3
