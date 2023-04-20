3aaa07e8941a8c634a7cf5036e1272768f0ac68b
unknown <ismayilrha@code.edu.az>
2023-04-20 21:11:32 +0400

    added assigment 3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("test");
+function transformString(inputString) {
+    let transformedString = '';
+    for (let i = 0; i < inputString.length; i++) {
+      if (i > 0) {
+        transformedString += '-';
+      }
+      transformedString += inputString[i].toUpperCase();
+      for (let j = 0; j < i; j++) {
+        transformedString += inputString[i];
+      }
+    }
+    return transformedString;
+  }
+
+
+  let result = transformString('hello');
+console.log(result); 
